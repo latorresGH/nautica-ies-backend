05cf8306b267e8a3e072be5eacba092defe71282
laTorresGH <laureano26112001@hotmail.com>
2025-08-08 20:26:33 -0300

    Inicializamos proyecto Spring Boot

---
+++ src/main/java/com/nautica/backend/nautica_ies_backend/NauticaIesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nautica.backend.nautica_ies_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauticaIesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NauticaIesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nautica/backend/nautica_ies_backend/NauticaIesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nautica.backend.nautica_ies_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauticaIesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
