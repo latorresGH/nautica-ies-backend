1390a28455c32469fe3c655484025c982d622222
laTorresGH <laureano26112001@hotmail.com>
2025-08-09 00:33:25 -0300

    Armamos estructura base

---
+++ src/main/java/com/nautica/backend/nautica_ies_backend/controllers/EntidadController.java
@@ -0,0 +1,5 @@
+package com.nautica.backend.nautica_ies_backend.controllers;
+
+public class EntidadController {
+    
+}
---
+++ src/main/java/com/nautica/backend/nautica_ies_backend/models/Entidad.java
@@ -0,0 +1,5 @@
+package com.nautica.backend.nautica_ies_backend.models;
+
+public class Entidad {
+    
+}
