21348018a6419e43e2df2fc00721008176f51e53
Agustin <agusmourrut@gmail.com>
2025-08-18 22:34:34 -0300

    Agregado de los enums

---
+++ src/main/java/com/nautica/backend/nautica_ies_backend/models/enums/EstadoPedido.java
@@ -0,0 +1,3 @@
+package com.nautica.backend.nautica_ies_backend.models.enums;
+
+public enum EstadoPedido { pendiente, entregado }
---
+++ src/main/java/com/nautica/backend/nautica_ies_backend/models/enums/TipoCliente.java
@@ -0,0 +1,3 @@
+package com.nautica.backend.nautica_ies_backend.models.enums;
+
+public enum TipoCliente { cliente, propietario, autorizado }
